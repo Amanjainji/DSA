100f23cf6000d080d46ec0833a819eb48955f872
Amanjainji <ajamanjain27@gmail.com>
2025-01-13 16:33:13 +0530

    dp add

--- Dynamic Programming/fibonacci.cpp
+++ Dynamic Programming/fibonacci.cpp
@@ -13,9 +13,20 @@ int fibonacci(int n,vector<int> &dp){
     return dp[n];
 }
 
+int fibonacci2(int n){
+    vector<int> dp(n+1,-1);
+    dp[0]=0;
+    dp[1]=1;
+    for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+    return dp[n];
+}
+
 
 int main(){
     int n=6;
     vector<int> dp(n+1,-1); 
-    cout<<fibonacci(n,dp);
+    cout<<fibonacci(n,dp)<<endl;
+    cout<<fibonacci2(n);
 }
