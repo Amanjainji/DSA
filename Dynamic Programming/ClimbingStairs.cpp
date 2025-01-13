44730e7f35828fba42a2d7a61453a905a8229032
Amanjainji <ajamanjain27@gmail.com>
2025-01-13 18:27:24 +0530

    dp add

--- Dynamic Programming/ClimbingStairs.cpp
+++ Dynamic Programming/ClimbingStairs.cpp
@@ -2,6 +2,43 @@
 #include<vector>
 using namespace std;
 
-int countDistinctWayToClimb(int nStairs){
-    
+//Top-Down Approach
+int solve(int nStairs,int i,vector<int> &dp){
+    //base case
+    if(i==nStairs)
+        return 1;
+    if(i>nStairs){
+        return 0;
+    }
+
+    dp[i]= solve(nStairs,i+1,dp) + solve(nStairs,i+2,dp);
+    return dp[i];
+}
+
+int countDistinctWayToClimb(int n){
+    vector<int> dp(n+1,-1);
+    return solve(n,0,dp);
+}
+
+
+//space optimized
+int climbStairs(int n) {
+        if (n <= 3) 
+            return n;
+
+        int prev1 = 3;
+        int prev2 = 2;
+
+        for (int i = 3; i < n; i++) {
+            int cur = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = cur;
+        }
+
+        return prev1; 
+}
+int main(){
+    int n=5;
+    cout<<countDistinctWayToClimb(n)<<endl;
+    cout<<climbStairs(n);
 }
