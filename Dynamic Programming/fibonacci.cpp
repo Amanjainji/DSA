fd82c01e76039f604bbc9fc52be11e34391462bf
Amanjainji <ajamanjain27@gmail.com>
2025-01-13 17:50:41 +0530

    dp add

--- Dynamic Programming/fibonacci.cpp
+++ Dynamic Programming/fibonacci.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 using namespace std;
 
+//Top down approach
 int fibonacci(int n,vector<int> &dp){
     if(n==0 || n==1){
         return n;
@@ -13,6 +14,7 @@ int fibonacci(int n,vector<int> &dp){
     return dp[n];
 }
 
+//Bottom down approach
 int fibonacci2(int n){
     vector<int> dp(n+1,-1);
     dp[0]=0;
@@ -23,10 +25,25 @@ int fibonacci2(int n){
     return dp[n];
 }
 
+//Space Optimization Approach
+int fibonacci3(int n){
+    int prev1=1;
+    int prev2=0;
+    if(n==0)
+        return 0;
+    for(int i=2;i<=n;i++){
+        int curr=prev1+prev2;
+        prev2=prev1;
+        prev1=curr;
+    }
+    return prev1;
+}
+
 
 int main(){
     int n=6;
     vector<int> dp(n+1,-1); 
     cout<<fibonacci(n,dp)<<endl;
-    cout<<fibonacci2(n);
+    cout<<fibonacci2(n)<<endl;
+    cout<<fibonacci3(n);
 }
