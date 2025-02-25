48f82fa2574d3aa5dc0e73561c302dca64dbed2d
Amanjainji <ajamanjain27@gmail.com>
2025-02-25 18:23:41 +0530

    new file

---
+++ BinaryTree/Leetcode889.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+//leetcode #889
+
+//algo 
+/*
+Create Root Node:
+
+Construct the root node from preorder[0] and mark it as visited in mpp and Store it in the node map.
+Iterate through Preorder List:
+
+For each node in preorder (starting from index 1):
+Create a TreeNode for the current value and mark it as visited in mpp.
+Store the node in the node map.
+Find Parent in Postorder:
+
+Determine the index of the current node in postorder.
+Check for the nearest visited ancestor in postorder (i.e., a node that has space for a left or right child and already found in preorder).
+Attach the Node:
+
+If the found ancestor has a free left child, attach the new node as the left child.
+Otherwise, attach it as the right child.
+Repeat Until Tree is Constructed:
+*/
+struct TreeNode {
+         int val;
+         TreeNode *left;
+         TreeNode *right;
+         TreeNode() : val(0), left(nullptr), right(nullptr) {}
+         TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+         TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+     };
+
+TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
+    unordered_map<int,bool> mpp;
+    unordered_map<int,TreeNode*> node;
+    for(int i=1;i<=preorder.size();i++){
+        mpp[i]=false;
+    }
+    unordered_map<int,int> indexPostOrder;
+    for(int i=0;i<postorder.size();i++){
+        indexPostOrder[postorder[i]]=i;
+    }
+    TreeNode* root=new TreeNode(preorder[0]);
+    mpp[preorder[0]]=true;
+    node[preorder[0]]=root;
+    for(int i=1;i<preorder.size();i++){
+        TreeNode* temp=new TreeNode(preorder[i]);
+        mpp[preorder[i]]=true;
+        node[preorder[i]]=temp;
+        int index=indexPostOrder[preorder[i]];
+        for(int j=index+1;j<postorder.size();j++){
+            if(mpp[postorder[j]]){
+                if(node[postorder[j]]->left && node[postorder[j]]->right)
+                    continue;
+                else{
+                    if(!node[postorder[j]]->left){
+                        node[postorder[j]]->left=temp;
+                    }
+                    else if(!node[postorder[j]]->right){
+                        node[postorder[j]]->right=temp;
+                    }
+                    break;
+                }
+            }
+        }
+    }
+    return root;
+}
---
+++ Dynamic Programming/2D DP/StoneGameII.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int> &piles,int i,int M,int alice){
+    if(i==piles.size())
+        return 0;
+    
+    int ans=alice?INT_MIN :INT_MAX;
+    int total=0;
+
+    for(int X=1;X<=2*M;++X){
+        if(i+X-1>=piles.size()) 
+            break;
+        total+=piles[i+X-1];
+        if(alice)
+            ans=max(ans,total+solve(piles,i+X,max(X,M),!alice));
+        else    
+            ans=min(ans,solve(piles,i+X,max(X,M),!alice));
+    }
+    return ans;
+}
+
+int stoneGameII(vector<int> &piles){
+    return solve(piles,0,1,true);
+}
+
+//Top down
+int solveMem(vector<int> &piles,int i,int M,int alice,vector<vector<vector<int>>> &dp){
+    if(i==piles.size())
+        return 0;
+    
+    if(dp[i][M][alice]!=-1)
+        return dp[i][M][alice];
+    int ans=alice?INT_MIN :INT_MAX;
+    int total=0;
+
+    for(int X=1;X<=2*M;++X){
+        if(i+X-1>=piles.size()) 
+            break;
+        total+=piles[i+X-1];
+        if(alice)
+            ans=max(ans,total+solveMem(piles,i+X,max(X,M),!alice,dp));
+        else    
+            ans=min(ans,solveMem(piles,i+X,max(X,M),!alice,dp));
+    }
+    return dp[i][M][alice]=ans;
+}
+
+int stoneGameIIMem(vector<int> &piles){
+    vector<vector<vector<int>>> dp(piles.size()+1,vector<vector<int>>(piles.size()+1,vector<int>(2,-1)));
+    return solveMem(piles,0,1,true,dp);
+}
+
+//Bottom Up
+int stoneGameII_Tab(vector<int> &piles){
+    vector<vector<vector<int>>> dp(piles.size()+1,vector<vector<int>>(piles.size()+1,vector<int>(2,0)));
+
+    for(int i=piles.size()-1;i>=0;--i){
+        for(int M=piles.size();M>=1;--M){
+            for(int alice=0;alice<=1;++alice){
+                int ans=alice?INT_MIN:INT_MAX;
+                int total=0;
+                for(int X=1;X<=2*M;++X){
+                    if(i+X-1>=piles.size()) break;
+                    total+=piles[i+X-1];
+                    if(alice)
+                        ans=max(ans,total+dp[i+X][max(X,M)][!alice]);
+                    else    
+                        ans=min(ans,dp[i+X][max(X,M)][!alice]);
+                }
+                dp[i][M][alice]=ans;
+            }
+        }
+    }
+    return dp[0][1][1];
+}
+int main(){
+    vector<int> piles={2,7,9,4,4};
+    cout<<stoneGameII_Tab(piles);
+}
