3d98b734d6aa194626c62e1b6c7859c3bbd2038b
Amanjainji <ajamanjain27@gmail.com>
2025-06-01 23:53:16 +0530

    Initial Commit

--- GreedyProblems/JobSequencing.cpp
+++ GreedyProblems/JobSequencing.cpp
@@ -13,7 +13,7 @@ Input: id = [1, 2, 3, 4], deadline = [4, 1, 1, 1], profit = [20, 1, 40, 30]
 Output: [2, 60]
 Explanation: Job1 and Job3 can be done with maximum profit of 60 (20+40).
 */
-
+/*
 static bool cmp(vector<int> a,vector<int> b){
     return a[0]>b[0];
 }
@@ -53,16 +53,72 @@ vector<int> JobSequencing(vector<int> &id, vector<int> &deadline,vector<int> &pr
 }
 
 int main(){
-    /*
+    
     vector<int> id = {1, 2, 3, 4}, deadline = {4, 1, 1, 1}, profit = {20, 10, 40, 30};
     vector<int> k=JobSequencing(id,deadline,profit);
     for(auto i: k){
         cout<< i <<" ";
-    }*/
+    }
 
     vector<int> id ={1,2,3,4,5}, deadline ={2,1,2,1 ,1},profit={100 ,19 ,27 ,25 ,15};
     vector<int> k=JobSequencing(id,deadline,profit);
     for(auto i: k){
         cout<< i <<" ";
     }
-}
+}*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+// A structure to represent a job 
+struct Job {
+   int id; // Job Id 
+   int dead; // Deadline of job 
+   int profit; // Profit if job is over before or on deadline 
+};
+class Solution {
+   public:
+      bool static comparison(Job a, Job b) {
+         return (a.profit > b.profit);
+      }
+   //Function to find the maximum profit and the number of jobs done
+   pair < int, int > JobScheduling(Job arr[], int n) {
+
+      sort(arr, arr + n, comparison);
+      int maxi = arr[0].dead;
+      for (int i = 1; i < n; i++) {
+         maxi = max(maxi, arr[i].dead);
+      }
+
+      int slot[maxi + 1];
+
+      for (int i = 0; i <= maxi; i++)
+         slot[i] = -1;
+
+      int countJobs = 0, jobProfit = 0;
+
+      for (int i = 0; i < n; i++) {
+         for (int j = arr[i].dead; j > 0; j--) {
+            if (slot[j] == -1) {
+               slot[j] = i;
+               countJobs++;
+               jobProfit += arr[i].profit;
+               break;
+            }
+         }
+      }
+
+      return make_pair(countJobs, jobProfit);
+   }
+};
+int main() {
+   int n = 4;
+   Job arr[n] = {{1,4,20},{2,1,10},{3,2,40},{4,2,30}};
+
+   Solution ob;
+   //function call
+   pair < int, int > ans = ob.JobScheduling(arr, n);
+   cout << ans.first << " " << ans.second << endl;
+
+   return 0;
+} 
