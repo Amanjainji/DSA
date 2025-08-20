f43db5881434e308ee640d2ec5f70f31536a53c9
Amanjainji <ajamanjain27@gmail.com>
2025-08-21 01:38:09 +0530

    files

---
+++ Maths/PrimeFactors.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> primeFactors(int n){
+    vector<int> ans;
+    for(int i=2;i<pow(n,0.5);i++){
+        if(n%i==0){
+            ans.push_back(i);
+            while(n%i==0)
+                n=n/i;
+        }      
+    }
+    if(n>1) ans.push_back(n);
+    return ans;
+}
+int main(){
+    vector<int> k=primeFactors(780);
+    for(auto i:k)
+        cout<<i<<endl;
+}
